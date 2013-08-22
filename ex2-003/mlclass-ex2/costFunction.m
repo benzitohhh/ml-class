592094159a90a6b683065445cf422d54f064f2bf
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2013-08-22 12:44:59 -0300

    finished logistic regresion yay

--- ex2-003/mlclass-ex2/costFunction.m
+++ ex2-003/mlclass-ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,16 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = X * theta; % predictions
+h = sigmoid(h) % apply sigmoid
+costY1 = y .* -log(h);
+costY0 = (1 - y) .* -log(1 - h);
+cost = costY0 + costY1;
+J = 1 / m * sum(cost);
 
-
-
-
-
-
+grad = (h - y)' * X;
+grad = 1 / m * grad;
+grad = grad';
 
 % =============================================================
 
