da35de3d5adca217e3bb0504597748625fc76799
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2013-10-03 13:21:39 +0100

    added note to sigmoid gradient

--- ex4-003/mlclass-ex4/randInitializeWeights.m
+++ ex4-003/mlclass-ex4/randInitializeWeights.m
@@ -23,11 +23,7 @@ W = zeros(L_out, 1 + L_in);
 epsilon_init = 0.12;
 W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 
-
-
-
-
-
+% i.e. the above ensures each val is in range [-epsilon, epsilon]
 
 % =========================================================================
 
