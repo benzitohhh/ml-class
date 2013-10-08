71a34928a090fe88d6c35e09f0a7f5541a28b964
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2013-10-08 12:42:57 +0100

    k-mean - closest centroid working

--- ex7-003/mlclass-ex7/findClosestCentroids.m
+++ ex7-003/mlclass-ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,19 +15,28 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
+m = size(X, 1);
+for i = 1:m
+    c = -1;
+    min_sqdist = intmax;
+    for j = 1:K
+        sqdist = X(i, :) - centroids(j, :);  % 1 x d
+        sqdist = sqdist .* sqdist;
+        sqdist = sum(sqdist, 2);               % 1 x 1
+        if (sqdist <= min_sqdist)
+           min_sqdist = sqdist;
+           c = j;
+        endif
+    end
+    idx(i) = c;
+end
 
 
 % =============================================================
 
 end
-
